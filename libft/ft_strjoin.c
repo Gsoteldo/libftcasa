88d336049479c3a5979bc1afeaa3dee2b388ec20
Gsoteldo <139263438+Gsoteldo@users.noreply.github.com>
2023-09-18 20:43:21 +0200

    Add files via upload

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsoteldo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 17:56:49 by gsoteldo          #+#    #+#             */
+/*   Updated: 2023/09/15 18:27:25 by gsoteldo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	j;
+
+	ptr = malloc(ft_strlen(s1) + ft_strlen(s2));
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+	ptr[i + j] = s2[j];
+	j++;
+	}
+	return (ptr);
+}

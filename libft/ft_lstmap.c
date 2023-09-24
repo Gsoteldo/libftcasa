3cfe379973fb30d225f8d9ba1c4b74f5d360bb17
Gabriel <gsoteldor@gmail.com>
2023-09-24 16:50:10 +0200

    Se terminan las funciones bonus y makefile. Falta terminar main de prueba

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -3,41 +3,28 @@
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list *new;
-    t_list *temp;
+    t_list *primero;
+    t_list *nuevo;
 
     if (lst == 0 || f == 0 || del == 0)
         return (0);
-    new = 0;
+    nuevo = 0;
     while (lst)
     {
-        temp = ft_lstnew(f(lst->content));
-        if (!temp)
+        if (!(nuevo = ft_lstnew(f(lst->content))))
         {
-            ft_lstclear(&new, del);
+            while (primero != 0)
+            {
+                nuevo = primero->next;
+                del(primero->content);
+                free(primero);
+                primero = nuevo;
+            }
+            lst = 0;
             return (0);
         }
-        ft_lstadd_back(&new, temp);
+        ft_lstadd_back(&primero, nuevo);
         lst = lst->next;
     }
-    return (new);
-}
-
-int main()
-{
-    t_list *lst;
-    t_list *new;
-
-    lst = ft_lstnew("first");
-    lst->next = ft_lstnew("second");
-    lst->next->next = ft_lstnew("third");
-    lst->next->next->next = ft_lstnew("fourth");
-    lst->next->next->next->next = ft_lstnew("fifth");
-    new = ft_lstmap(lst, ft_lstadd_back, ft_lstdelone);
-    while (new)
-    {
-        printf("%s\n", (char *)new->content);
-        new = new->next;
-    }
-    return (0);
+    return (primero);
 }
---
+++ libft/test.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+#include <stdio.h>
+
+int main()
+{
+    //1: Prueba de ft_isalpha
+    printf("1: Prueba de ft_isalpha: \n");
+    printf("a: %d\n", ft_isalpha('a'));
+    printf("A: %d\n", ft_isalpha('A'));
+    printf("z: %d\n", ft_isalpha('z'));
+    printf("Z: %d\n", ft_isalpha('Z'));
+    printf("0: %d\n", ft_isalpha('0'));
+    printf("9: %d\n", ft_isalpha('9'));
+    printf("+: %d\n", ft_isalpha('+'));
+    printf("=: %d\n", ft_isalpha('='));
+    printf("?: %d\n", ft_isalpha('?'));
+    printf("!: %d\n", ft_isalpha('!')); 
+
+}
